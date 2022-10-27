5eeda5911cde797448b8de27c183e2bdadd2790c
HEBERT SANTANA <hebert.santana@senacsp.edu.br>
2022-10-26 22:20:48 -0300

    CRUD - Listar, Incluir e Apagar OK

---
+++ banco de dados/formulário/envia.php
@@ -0,0 +1,34 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <form action="recebe.php" method="post">
+        <label for="nome">nome:</label>
+        <input type="text" name="nomeUsuario" id ="nome"><br>
+        <label for="idade">idade:</label>
+        <input type="number" name="idadeUsuario" id ="Usuario"><br>
+        <label for="cpf">CPF:</label>
+        <input type="number" name="cpfUsuario" id ="cpf"><br>
+        <label for="periodo">PERIODO:</label>
+        <select name="periodoUsuario" id="periodo">
+            <option value="">escolha</option>
+            <option value="matutino">Matutino</option>
+            <option value="vespertino">Vespertino</option>
+            <option value="noturno">Noturno</option>
+            <input type="submit" value="Enviar">
+
+
+
+        </select>
+   
+
+    </form>    
+    
+</body>
+</html>
+
---
+++ crud/view/listar.php
@@ -0,0 +1,55 @@
+<?php
+
+//BUSCANDO O COD DA CONEXAO COM O BANCO DE DADOS
+
+require_once '../BancoDeDados/conecta.php';
+//include(); incluir 
+//include_once(); nao gera erro fatal se nao existir
+
+// DADOS DO HTML
+
+$nome = $_POST['nome'];
+$turno = $_POST['turno'];
+$inicio = $_POST['inicio'];
+
+$consulta = $bd->prepare('INSERT INTO alunos
+                        (nome, turno, inicio)
+                        VALUES 
+                        (:nome ,:turno, :inicio)');
+/*
+o objstmt esta retornando um array que contem um
+ pre consulta no banco de dados (sem os dados do usuario)
+ ----
+ A funcao $bd->prepare() retorna
+ outra variavel (objeto), essa outra 
+ variavel consegue juntar os dados 
+ do usuario com consulta SQL
+*/
+
+$consulta->bindParam('nome', $nome);
+$consulta->bindParam('turno', $turno);
+$consulta->bindParam('inicio', $inicio);
+
+/*
+
+'procurar', $substituirPor
+
+A funcao $consulta->bindParam() substitui
+os rotulos (ex.: ":nome") pelos dados inseguros
+ */
+
+ if( $consulta->execute()){
+
+    echo "Gravado com sucesso!";
+
+ }else{
+
+    echo "ERRO ao gravar no banco de dados";
+
+ }
+
+ //Finalmente executamos a consulta no BD
+
+ include 'index.php';
+
+
